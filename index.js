03c2ac653d2feefe0a4e1b585efa85f29f40a028
brightkingpin <luvandabright781@gmail.com>
2023-03-18 18:00:59 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// Define the divide function
+function divide() {
+    return 2000 / 100;
+  }
+  
+// Define the square arrow function
+const square = (x) => {
+    return x * x;
+  }
+  
+// Define the add arrow function
+const add = (a, b) => {
+    return a + b;
+  }
+  
